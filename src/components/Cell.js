cd1648281465fee7ab3e4dc73e076bd2ab3c5795
Phuc. Tran Trong (4) <phuctt4@vng.com.vn>
2019-06-25 17:51:28 +0700

    update

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Cell = ({value}) => (
+    
+    <ReactCSSTransitionGroup
+    transitionName="example"
+    transitionEnterTimeout={500}
+    transitionLeaveTimeout={300}>
+
     <div className={`cell color-${value}`}>
         {value === 0 ? '' : value}
     </div>
+  </ReactCSSTransitionGroup>
+    
 )
 
 Cell.propTypes = {
--- src/reducers/cells.js
+++ src/reducers/cells.js
@@ -10,6 +10,10 @@ const generate = () => {
     return board
 }
 
+const getSize = (board) => {
+    return Math.floor(Math.sqrt(board.length));
+}
+
 const generateRandom = (board) => {
 
     if(isFull(board)) {
@@ -29,21 +33,88 @@ const isFull = (board) => {
     return true;
 }
 
+const LEFT = {x: 0, y: -1}
+const UP = {x: -1, y: 0}
+const RIGHT = {x: 0, y: 1}
+const DOWN = {x: 1, y: 0}
+
+
+const move = (board, action) => {
+    const size = getSize(board)
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            if(board[i * size + j].value !== 0) {
+                let x = i + action.x
+                let y = j + action.y
+
+                while(x >= 0 && x < size && y >=0 && y < size && board[x * size + y].value === 0) {
+                    x += action.x
+                    y += action.y
+                }
+                
+
+                x = x - action.x
+                y = y - action.y
+
+                board[x * size + y].value = board[i * size + j].value
+                if(i !== x || j !== y)
+                    board[i * size + j].value = 0
+            } 
+        }
+    }
+    console.log('move')
+}
+
+const join = (board, action) => {
+    const size = getSize(board)
+    const merge = (board, i , j) => {
+        if(board[i * size + j] !== 0) {
+            let x = i + action.x
+            let y = j + action.y
+
+            if(x >= 0 && x < size && y >=0 && y < size && board[i * size + j].value === board[x * size + y].value) {
+                board[x * size + y].value *= 2
+                board[i * size + j].value = 0
+            }
+        }
+    }
+
+    if(UP === action || LEFT === action) {
+        for(let i = 0; i < size; i++) {
+            for(let j = 0; j < size; j++) {
+                merge(board, i, j)
+            }
+        }
+    } else {
+        for(let i = size - 1; i >= 0; i--) {
+            for(let j = size - 1; j >= 0; j--) {
+                merge(board, i, j);
+            }
+        }
+    }
+    console.log('join')
+}
+
 const plusCell = (board, action) => {
     // let size = 4
     // let current = {x: size - 1, y: 0}
-    
-    return board;
+    move(board, action)
+    join(board, action)
+    move(board, action)
+    return board
 } 
 
 
 const cells = (state = generate(), action) => {
     switch (action.type) {
         case 'LEFT':
+            return generateRandom(plusCell([...state], LEFT))
         case 'UP':
+            return generateRandom(plusCell([...state], UP))
         case 'RIGHT':
+            return generateRandom(plusCell([...state], RIGHT))
         case 'DOWN':
-            return generateRandom(plusCell([...state], action.type));
+            return generateRandom(plusCell([...state], DOWN))
         default:
             return state;
     }
